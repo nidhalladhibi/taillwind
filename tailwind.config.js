a98ffa1509ce9c7f50d28b51011f44b26ffefe52
ladhibi nidhal <www.nidhal.com@gmail.com>
2024-10-29 15:15:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
